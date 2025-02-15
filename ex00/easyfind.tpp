af795ae4a037517f1e24bbf5aaea534c02036ac2
Andrei <krestonosa@gmail.com>
2025-02-15 17:06:08 +0100

    Starting with algorithm library

---
+++ ex00/easyfind.tpp
@@ -0,0 +1,7 @@
+#include "easyfind.hpp"
+#include <algorithm>
+
+template<typename T>
+T easyfind(T array, int to_find){
+
+}
---
+++ ex00/include/easyfind.hpp
@@ -0,0 +1,7 @@
+# ifndef EASY_FIND_HPP
+#define EASY_FIND_HPP
+
+template<typename T>
+T easyfind(T array, int to_find);
+
+#endif
