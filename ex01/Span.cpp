4f8c3c85399b50ac6701ad4d36134fdc2af5b5ec
Andrei <krestonosa@gmail.com>
2025-02-18 18:42:04 +0100

    Working with multiset container as it has build in sorted system. Important lessons from today : containers like multiset has intitialization so when working with the class that has multiset container i dont need to create new instance for copy/=operator as it will be done automatically. Additionally trying function that takes template <typename T> as vector,int array, list etc and adds it to the current container

--- ex01/Span.cpp
+++ ex01/Span.cpp
@@ -1,1 +1,62 @@
 #include "Span.hpp"
+
+Span::Span() : _maxSize(0), _numbers() {}
+
+Span::Span(unsigned int max_size) : _maxSize(max_size), _numbers() {
+    if (_numbers.max_size() < max_size)
+        throw MaxNumberException();
+}
+Span::Span(const Span &copy) : _size(copy._size), _numbers(copy._numbers) {}
+Span &Span::operator = (const Span &source) {
+    if (this != &source) {
+        this->_maxSize = source._maxSize;
+        this->_numbers = source._numbers;
+    }
+    return *this;
+}
+Span~Span() {}
+
+template <typename T>
+void            addNumbers(const T& container) {
+    if (sizeof(container) != sizeof(int)) {
+        throw IncorrectType();
+    }
+    if (container.size() + _numbers.size() > _maxSize 
+        || container.size() + _numbers.size() > _numbers.max_size())
+        throw MaxNumberException();
+    _numbers.insert_range(container.begin(), container.end());
+}
+
+void            Span::addNumber(int n) {
+    if (_numbers.size() + 1  > _maxSize || _numbers.size() + 1 > _numbers.max_size())
+        throw MaxNumberException();
+    else {
+        _numbers.insert(n);
+    }
+}
+void            addRandomNumbers(unsigned int n) {
+    if (_numbers.size() + n  > _maxSize || _numbers.size() + n > _numbers.max_size())
+        throw MaxNumberException();
+    srand(time(NULL));
+    for (unsigned int i = 0; i < n; ++i) {
+
+    }
+}
+
+std::multiset   Span::getNumbers() const {
+    return _numbers;
+}
+
+unsigned int    Span::getMaxSize() const {
+    return _maxSize;
+}
+
+// additional test if the numbers inside multiset are duplicates? 
+unsigned int    shortestSpan() const {
+    if (_numberes.size() == 1 || _numberes.size() == 0)
+        throw NoSpanException();
+}
+unsigned int    longestSpan() const {
+    if (_numberes.size() == 1 || _numberes.size() == 0)
+        throw NoSpanException();
+}
--- ex01/include/ Span.hpp
+++ ex01/include/ Span.hpp
@@ -4,34 +4,47 @@
 
 class Span {
 private:
-    unsigned int _maxInt;
-
+    unsigned int    _maxSize;
+    std::multiset   _numbers;
 public:
     Span();
-    Span(unsigned int max_int);
+    Span(unsigned int max_size);
     Span(const Span &copy);
     Span &operator = (const Span &source);
     ~Span();
 
-    void addNumber();
-    void addNumbers();
-    shortestSpan();
-    longestSpan();
+    template <typename T>
+    void            addNumbers(const T& container);
+    void            addNumber(int n);
+    // addRandomNumbers should take n numbers to add container
+    void            addRandomNumbers(unsigned int n);
 
-};
+    std::multiset   getNumbers() const;
+    unsigned int    getMaxSize() const;
+    unsigned int    shortestSpan() const;
+    unsigned int    longestSpan() const;
 
-class   MaxNumberException : public std::exception {
-    public:
-        virtual const char *what() const throw() {
-            return "Max number of elements";
-        }   
-};
 
-class   NoSpanException : public std::exception {
-    public:
-        virtual const char *what() const throw() {
-            return "No Span can be found";
-        }
+    class   MaxNumberException : public std::exception {
+        public:
+            virtual const char *what() const throw() {
+                return "Max number of elements";
+            }   
+    };
+    
+    class   NoSpanException : public std::exception {
+        public:
+            virtual const char *what() const throw() {
+                return "No Span can be found";
+            }
+    };
+
+    class   IncorrectType : public std::exception {
+        public:
+            virtual const char *what() const throw() {
+                return "No Span can be found";
+            }
+    };
 };
 
 #endif
